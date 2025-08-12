0e94cffd92eb9909f44f0d723dc826ee7925a77d
JonesOmari00 <jonesomari00@outlook.com>
2025-08-12 12:02:13 -0700

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+//HelloWorld.java
+// omarij 8/12/25
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.printf("Hello World it's Omari Jones!");
+    }
+}
